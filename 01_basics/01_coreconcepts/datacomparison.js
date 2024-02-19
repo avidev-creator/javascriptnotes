f92694c605879f6e6b8a0f97f41155dfa8c40866
Avinash Dubey <123487788+avidev-creator@users.noreply.github.com>
2024-02-19 08:53:24 +0530

    restructure

---
+++ 01_basics/01_coreconcepts/datacomparison.js
@@ -0,0 +1,30 @@
+
+
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 = 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 === 1);
+// console.log(2 != 1);
+
+//Trick Questions for Interview
+
+console.log(null > 0);  // false
+console.log(null < 0);  // false
+console.log(null >= 0); // true
+console.log(null <= 0); // true
+console.log(null == 0); // false
+
+console.log(undefined > 0);  // false
+console.log(undefined < 0);  // false
+console.log(undefined >= 0); // false
+console.log(undefined <= 0); // flase
+console.log(undefined == 0); // false
+
+console.log("1"+2);   // If the starting value is a String the whole data will be treated as string.
+console.log("1"+2+3); // If the starting value is a String the whole data will be treated as string.
+console.log(1+2+"3"); // If the starting value is a number the operation will happen till it find a string and than convert the result to string.
+
+
+
