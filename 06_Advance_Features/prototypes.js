186f8a4257114a724f720285ae334ee6c40dfe1a
Avinash Dubey <123487788+avidev-creator@users.noreply.github.com>
2024-02-22 18:09:19 +0000

    final

--- 06_Advance_Features/prototypes.js
+++ 06_Advance_Features/prototypes.js
@@ -24,3 +24,67 @@ The constructor is called: The constructor function is called with the specified
 The new object is returned: After the constructor function has been called, if it doesn't return a non-primitive value (object, array, function, etc.), the newly created object is returned.
 
 */
+
+// This is to inject prototypes inside the core like Object, arrays ------
+
+let myHeros = ["thor", "spiderman"]
+
+
+let heroPower = {
+    thor: "hammer",
+    spiderman: "sling",
+
+    getSpiderPower: function(){
+        console.log(`Spidy power is ${this.spiderman}`);
+    }
+}
+
+Object.prototype.avi = function(){
+    console.log(`Avinash is present in all objects`);
+}
+
+Array.prototype.heyAvi = function(){
+    console.log(`Avi says hello`);
+}
+
+// heroPower.avi()
+// myHeros.avi()
+// myHeros.heyAvi()
+// heroPower.heyAvi()
+
+// Prototypal inheritance
+
+const User = {
+    name: "avinash",
+    email: "example@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+// modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+let anotherUsername = "Avinash     "
+
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    console.log(`True length is: ${this.trim().length}`);
+}
+
+anotherUsername.trueLength()
+"summit".trueLength()
+"coffee".trueLength()
