08d75bf9f07cf50e88edc91dd6f7ef18270dc86c
Avinash Dubey <123487788+avidev-creator@users.noreply.github.com>
2024-02-22 09:34:53 +0530

    Update fetch.js

--- 05_promise/fetch.js
+++ 05_promise/fetch.js
@@ -1,3 +1,5 @@
+// Fetch with Promise --------------------------------------
+
 // const fetchPromise = fetch(
 //   "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
 // );
@@ -9,6 +11,8 @@
 // });
 
 // console.log("Started request…");
+
+// Fetch with multiple .then ------------------------------------
 
 fetch(
   "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
@@ -31,3 +35,50 @@ fetch(
   .then((filteredResult) => {
     console.log(filteredResult);
   });
+
+// Fetch with multiple .then and using filter function -------------
+
+fetch(
+  "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
+)
+  .then((res) => {
+    // console.log(res);
+    return res.json();
+  })
+  .then((data) => {
+    // console.log(data);
+    return data;
+  })
+  .then((result) => {
+    console.log(result);
+    let newResponse = result.filter(
+      (product) => product.name === "corned beef"
+    );
+    return newResponse;
+  })
+  .then((filteredResult) => {
+    console.log(filteredResult);
+  });
+
+
+// Fetch with async and await ---------------------------------
+
+async function fetchdata() {
+  const rawdata = await fetch(
+    "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
+  );
+
+  const jsondata = await rawdata.json();
+  const processesData = jsondata.filter(
+    (product) => product.name === "corned beef"
+  );
+
+  let objData;
+  for (const items of processesData) {
+    objData = items;
+  }
+
+  console.log(objData.name);
+}
+
+fetchdata();
